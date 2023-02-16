ddb54b2ac10596dd7d9ad313b739627c020d1f95
charie1 <charityyeboah53@gmail.com>
2023-02-16 14:26:58 +0000

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - using the puts function
+*
+*Return: success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
