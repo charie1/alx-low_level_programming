550572b9e6a49ab2b1df952a2c98e138e21bc9a9
charie1 <charityyeboah53@gmail.com>
2023-02-17 20:42:54 +0000

    did it myself

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - is the if loop function
+*
+* Return: Success
+*/
+
+int main(void)
+{
+	int num = 0;
+	
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+
+	putchar(10);
+	return (0);
+}
