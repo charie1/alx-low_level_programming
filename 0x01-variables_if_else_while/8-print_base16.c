f6e6bc4d08ca8fb200e4a0a25c8d60c47a7612ee
charie1 <charityyeboah53@gmail.com>
2023-02-17 22:28:48 +0000

    ....

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,9 +10,9 @@ int main(void)
 	int num;
 	char alp;
 
-	for (num = 0; num <= 9; num++)
+	for (num = 0; num < 10; num++)
 	{
-		putchar(num);
+		putchar(num + '0');
 	}
 
 	for (alp = 'a'; num <= 'f'; alp++)
