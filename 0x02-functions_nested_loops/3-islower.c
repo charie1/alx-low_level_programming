e70c9c8527b3c5631eae267973e93ad5e17273a1
charie1 <charityyeboah53@gmail.com>
2023-02-21 22:17:42 +0000

    trial and error

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,15 @@
 #include "header.h"
 /**
 * _islower - print number 1 for lowercase letter 1
-*
+*@c: the alphabet and varible c
 * Return: Succes(0)
 */
 
 int _islower(int c)
 {
-	for ( c = 'a'; c <= 'z' c++)
+	for (c = 'a'; c <= 'z' c++)
 	{
-		if ( c = 'c')
+		if (c == 'c')
 			_putchar('1');
 		else
 			_putchar('0');
