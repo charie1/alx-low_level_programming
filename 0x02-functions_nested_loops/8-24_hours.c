645687dcaed192a2d1c700c4aa25bf2130bb2c8d
charie1 <charityyeboah53@gmail.com>
2023-02-22 01:02:17 +0000

    24hrs

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,10 +13,10 @@ void jack_bauer(void)
 	{
 		_putchar((hr / 10) + '0');
 		_putchar((hr % 10) + '0');
-		_putchar(':');
 
 		for (min = 0; min <= 59; min++)
 		{
+			_putchar(':');
 			_putchar((min / 10) + '0');
 			_putchar((min % 10) + '0');
 			_putchar('\n');
