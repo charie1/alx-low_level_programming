3e18978585917cc84d3acb70de6895d73ab6b8c9
charie1 <charityyeboah53@gmail.com>
2023-02-22 01:53:18 +0000

    multiply

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* add - adds two integers
+*
+* @a: this is the first interger
+* @b: thi is the last integer
+* Return: success
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
