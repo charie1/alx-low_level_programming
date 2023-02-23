c5b57248f99f7ffca5b85ea98319bfc2dde0ecb9
charie1 <charityyeboah53@gmail.com>
2023-02-23 22:53:42 +0000

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isdigit - checks the digit typed.
+ * @a: the num 1 to be typed
+ * @b: the num 2 to be typed
+ *
+ * Return: Success(0).
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
