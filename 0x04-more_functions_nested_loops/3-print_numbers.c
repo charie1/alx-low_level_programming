e8ba1f053323f30c96ccb1d746bfac7707b119ae
charie1 <charityyeboah53@gmail.com>
2023-02-23 23:30:29 +0000

    it

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - display the number typed
+ * @num: digits
+ * Return: Success (0)
+*/
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num >= '0'; num <= '9'; num++)
+		_putchar(num);
+
+	_putchar('\n');
+}
