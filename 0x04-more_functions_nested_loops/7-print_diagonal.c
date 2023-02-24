87973e9e0c2901ecb191eb787181c1e5c6644a92
charie1 <charityyeboah53@gmail.com>
2023-02-24 05:31:11 +0000

    d

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_diagonal - checks the digit typed.
+ * @n: the num to be typed
+ * @y: the line to be drawn
+ * Return: Success(0).
+ */
+
+void print_diagonal(int n)
+{
+	int y;
+
+	if (n > 0)
+	
+	{
+		for (y = 0; y = n; y++)
+										_putchar ('#');
+								}
+
+	else
+					                		_putchar('\n');
+}
