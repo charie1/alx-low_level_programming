d76a855a427f13ece4d39934c96a6bd17f793c56
charie1 <charityyeboah53@gmail.com>
2023-02-24 00:37:43 +0000

    g

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_line - checks the digit typed.
+ * @n: the num to be typed
+ *
+ * Return: Success(0).
+ */
+
+void print_line(int n)
+{
+	int y;
+
+	if (n > 0)
+	{
+		for (y = 0; y < n; y++)
+			_putchar ('_');
+	}
+	else
+		_putchar('\n');
+}
+	
