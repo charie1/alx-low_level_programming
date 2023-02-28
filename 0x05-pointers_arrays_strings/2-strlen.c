e8c4c488133d2bc3e5bfe9f729c4f6802e8462cb
charie1 <charityyeboah53@gmail.com>
2023-02-28 20:36:30 +0000

    length of an arra

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,21 +1,18 @@
 #include "main.h"
+#include <stdio.h>
 /**
 * _strlen - returns the length of a string
 * @s: the string to get the length of.
 *
-* Return: the length of @str
+* Return: the length of @s
 */
 
 int _strlen(char *s)
 {
 	int num = 0;
 
-	if (s == " ")
-		num--;
-	else
+	while (*s++)
 		num++;
-	if (output == EOF)
-	{
-		return (num);
-	}
+
+	return (num);
 }
