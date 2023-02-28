fc9a93b5767294296744585dddeb13d96d5a175c
charie1 <charityyeboah53@gmail.com>
2023-02-28 20:08:10 +0000

    length of an array

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* _strlen - returns the length of a string
+* @s: the string to get the length of.
+*
+* Return: the length of @str
+*/
+
+int _strlen(char *s)
+{
+	int num = 0;
+
+	if (s == " ")
+		num--;
+	else
+		num++;
+	if (output == EOF)
+	{
+		return (num);
+	}
+}
