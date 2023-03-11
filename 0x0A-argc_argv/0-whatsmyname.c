8ab8eca00133fa3b2a541173fe66891e2c5ca167
charie1 <charityyeboah53@gmail.com>
2023-03-11 15:47:36 +0000

    task 0 - argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - print the arguments after the execution of the program
+* @argc: count the number of arguments passed
+* @argv: tells the arrays of characters passed.
+*
+* Return: success.
+**/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
