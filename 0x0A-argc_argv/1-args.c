cce17c7c421f99c8583ca88cbe7af9fba4b63883
charie1 <charityyeboah53@gmail.com>
2023-03-11 16:17:25 +0000

    task 1 - argc..

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,7 +7,7 @@
 * Return: success.
 **/
 
-int main(int argc, char __attrbute__((__unused__)) * argv[])
+int main(int argc, char __attribute__((__unused__)) * argv[])
 {
 	printf("%d\n", argc - 1);
 
