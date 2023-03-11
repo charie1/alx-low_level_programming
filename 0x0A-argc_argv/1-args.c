faba907bf2002628f1614e5b5165eebea93425ea
charie1 <charityyeboah53@gmail.com>
2023-03-11 16:09:27 +0000

    task 1 - argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - tells the command line argument passed to it
+* @argc: the first command line argument
+* @argv: the secomd command line argument
+*
+* Return: success.
+**/
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
