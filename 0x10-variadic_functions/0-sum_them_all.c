6be4f0983432054e95313800063f186dfb1db827
charie1 <charityyeboah53@gmail.com>
2023-03-23 19:21:56 +0000

    task 0 - alx variadic functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,7 +3,7 @@
 /**
 * sum_them_all - sums all the numbers inputted.
 * @n: the num of variadic arguments to be passed.
-* 
+*
 * Return: success
 **/
 
@@ -13,15 +13,15 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int count;
 	unsigned int sum = 0;
 
-	va_start (myNum, n);
+	va_start(myNum, n);
 
 	if (n == 0)
 		return (0);
 
 	for (count = 0; count < n; count++)
 	sum = sum + (va_arg(myNum, int));
-				    
-	va_end (myNum);
+
+	va_end(myNum);
 
 	return (sum);
 }
